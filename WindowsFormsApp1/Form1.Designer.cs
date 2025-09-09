07d7302f68de22a388a86ce704ca5a6344e854ef
unknown <dtvinh.a1.20.21@gmail.com>
2025-09-10 00:17:38 +0700

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class EnglishAcademy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnglishAcademy));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btn_equip = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_Ass = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_teacher = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_student = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_home = new Guna.UI2.WinForms.Guna2Button();
+            this.panelMain = new Guna.UI2.WinForms.Guna2Panel();
+            this.picBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.btn_equip);
+            this.guna2Panel1.Controls.Add(this.btn_Ass);
+            this.guna2Panel1.Controls.Add(this.btn_teacher);
+            this.guna2Panel1.Controls.Add(this.btn_student);
+            this.guna2Panel1.Controls.Add(this.btn_home);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(41)))), ((int)(((byte)(123)))));
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(204, 649);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // btn_equip
+            // 
+            this.btn_equip.Animated = true;
+            this.btn_equip.AutoRoundedCorners = true;
+            this.btn_equip.BackColor = System.Drawing.Color.Transparent;
+            this.btn_equip.BorderThickness = 2;
+            this.btn_equip.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_equip.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_equip.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_equip.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_equip.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_equip.ForeColor = System.Drawing.Color.White;
+            this.btn_equip.Location = new System.Drawing.Point(23, 441);
+            this.btn_equip.Name = "btn_equip";
+            this.btn_equip.Size = new System.Drawing.Size(158, 47);
+            this.btn_equip.TabIndex = 4;
+            this.btn_equip.Text = "Equipment";
+            this.btn_equip.Click += new System.EventHandler(this.btn_equip_Click);
+            // 
+            // btn_Ass
+            // 
+            this.btn_Ass.Animated = true;
+            this.btn_Ass.AutoRoundedCorners = true;
+            this.btn_Ass.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Ass.BorderThickness = 2;
+            this.btn_Ass.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Ass.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Ass.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Ass.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Ass.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ass.ForeColor = System.Drawing.Color.White;
+            this.btn_Ass.Location = new System.Drawing.Point(23, 360);
+            this.btn_Ass.Name = "btn_Ass";
+            this.btn_Ass.Size = new System.Drawing.Size(158, 47);
+            this.btn_Ass.TabIndex = 3;
+            this.btn_Ass.Text = "Assignment";
+            this.btn_Ass.Click += new System.EventHandler(this.btn_Ass_Click);
+            // 
+            // btn_teacher
+            // 
+            this.btn_teacher.Animated = true;
+            this.btn_teacher.AutoRoundedCorners = true;
+            this.btn_teacher.BackColor = System.Drawing.Color.Transparent;
+            this.btn_teacher.BorderThickness = 2;
+            this.btn_teacher.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_teacher.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_teacher.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_teacher.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_teacher.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_teacher.ForeColor = System.Drawing.Color.White;
+            this.btn_teacher.Location = new System.Drawing.Point(23, 279);
+            this.btn_teacher.Name = "btn_teacher";
+            this.btn_teacher.Size = new System.Drawing.Size(158, 47);
+            this.btn_teacher.TabIndex = 2;
+            this.btn_teacher.Text = "Teacher";
+            this.btn_teacher.Click += new System.EventHandler(this.btn_teacher_Click);
+            // 
+            // btn_student
+            // 
+            this.btn_student.Animated = true;
+            this.btn_student.AutoRoundedCorners = true;
+            this.btn_student.BackColor = System.Drawing.Color.Transparent;
+            this.btn_student.BorderThickness = 2;
+            this.btn_student.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_student.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_student.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_student.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_student.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_student.ForeColor = System.Drawing.Color.White;
+            this.btn_student.Location = new System.Drawing.Point(23, 198);
+            this.btn_student.Name = "btn_student";
+            this.btn_student.Size = new System.Drawing.Size(158, 47);
+            this.btn_student.TabIndex = 1;
+            this.btn_student.Text = "Student";
+            this.btn_student.Click += new System.EventHandler(this.btn_student_Click);
+            // 
+            // btn_home
+            // 
+            this.btn_home.Animated = true;
+            this.btn_home.AutoRoundedCorners = true;
+            this.btn_home.BackColor = System.Drawing.Color.Transparent;
+            this.btn_home.BorderThickness = 2;
+            this.btn_home.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_home.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_home.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_home.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_home.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_home.ForeColor = System.Drawing.Color.White;
+            this.btn_home.Location = new System.Drawing.Point(23, 117);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Size = new System.Drawing.Size(158, 47);
+            this.btn_home.TabIndex = 0;
+            this.btn_home.Text = "Home";
+            this.btn_home.Click += new System.EventHandler(this.btn_home_Click);
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelMain.BackgroundImage")));
+            this.panelMain.Controls.Add(this.label1);
+            this.panelMain.Controls.Add(this.guna2Panel2);
+            this.panelMain.Controls.Add(this.picBox);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.FillColor = System.Drawing.Color.White;
+            this.panelMain.Location = new System.Drawing.Point(204, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(795, 649);
+            this.panelMain.TabIndex = 1;
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Transparent;
+            this.picBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
+            this.picBox.ImageRotate = 0F;
+            this.picBox.Location = new System.Drawing.Point(0, 0);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(795, 649);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 20;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(795, 60);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WHAT THE FUCK, WHO IS ADMIN???";
+            // 
+            // EnglishAcademy
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(999, 649);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EnglishAcademy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "English Academy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel panelMain;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Button btn_home;
+        private Guna.UI2.WinForms.Guna2Button btn_equip;
+        private Guna.UI2.WinForms.Guna2Button btn_Ass;
+        private Guna.UI2.WinForms.Guna2Button btn_teacher;
+        private Guna.UI2.WinForms.Guna2Button btn_student;
+        private Guna.UI2.WinForms.Guna2PictureBox picBox;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class EnglishAcademy : Form
+    {
+        public EnglishAcademy()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_student_Click(object sender, EventArgs e)
+        {
+            // Xóa control cũ trong panel (nếu có)
+            panelMain.Controls.Clear();
+
+            // Khởi tạo form2
+            Form2 frm = new Form2();
+            frm.TopLevel = false;              // Quan trọng: không cho form chạy độc lập
+            frm.FormBorderStyle = FormBorderStyle.None; // Bỏ viền
+            frm.Dock = DockStyle.Fill;         // Fill toàn bộ panel
+
+            // Thêm form2 vào panelMain
+            panelMain.Controls.Add(frm);
+            frm.Show();
+        }
+
+        private void btn_teacher_Click(object sender, EventArgs e)
+        {
+            // Xóa control cũ trong panel (nếu có)
+            panelMain.Controls.Clear();
+
+            // Khởi tạo form2
+            Form3 frm = new Form3();
+            frm.TopLevel = false;              // Quan trọng: không cho form chạy độc lập
+            frm.FormBorderStyle = FormBorderStyle.None; // Bỏ viền
+            frm.Dock = DockStyle.Fill;         // Fill toàn bộ panel
+
+            // Thêm form2 vào panelMain
+            panelMain.Controls.Add(frm);
+            frm.Show();
+        }
+
+        private void btn_Ass_Click(object sender, EventArgs e)
+        {
+            // Xóa control cũ trong panel (nếu có)
+            panelMain.Controls.Clear();
+
+            // Khởi tạo form2
+            Form4 frm = new Form4();
+            frm.TopLevel = false;              // Quan trọng: không cho form chạy độc lập
+            frm.FormBorderStyle = FormBorderStyle.None; // Bỏ viền
+            frm.Dock = DockStyle.Fill;         // Fill toàn bộ panel
+
+            // Thêm form2 vào panelMain
+            panelMain.Controls.Add(frm);
+            frm.Show();
+        }
+
+        private void btn_equip_Click(object sender, EventArgs e)
+        {
+            // Xóa control cũ trong panel (nếu có)
+            panelMain.Controls.Clear();
+
+            // Khởi tạo form2
+            Form5 frm = new Form5();
+            frm.TopLevel = false;              // Quan trọng: không cho form chạy độc lập
+            frm.FormBorderStyle = FormBorderStyle.None; // Bỏ viền
+            frm.Dock = DockStyle.Fill;         // Fill toàn bộ panel
+
+            // Thêm form2 vào panelMain
+            panelMain.Controls.Add(frm);
+            frm.Show();
+        }
+
+        private void btn_home_Click(object sender, EventArgs e)
+        {
+            panelMain.Controls.Clear();
+            picBox.BringToFront();
+        }
+    }
+}
